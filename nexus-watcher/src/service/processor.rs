ee42a984e7c012898b9f89d79cb96dd49a1b09bf
ok300 <106775972+ok300@users.noreply.github.com>
2025-11-10 14:29:57 +0100

    Remove need for reqwest

--- nexus-watcher/src/service/processor.rs
+++ nexus-watcher/src/service/processor.rs
@@ -2,12 +2,13 @@ use crate::events::errors::EventProcessorError;
 use crate::events::retry::event::RetryEvent;
 use crate::events::{Event, Moderation};
 use crate::service::traits::TEventProcessor;
+use nexus_common::db::PubkyClient;
 use nexus_common::models::homeserver::Homeserver;
 use nexus_common::types::DynError;
 use opentelemetry::trace::{FutureExt, Span, TraceContextExt, Tracer};
 use opentelemetry::{global, Context, KeyValue};
+use pubky::Method;
 use pubky_app_specs::PubkyId;
-use std::error::Error;
 use std::path::PathBuf;
 use std::sync::Arc;
 use tokio::sync::watch::Receiver;
@@ -62,16 +63,19 @@ impl EventProcessor {
         debug!("Polling new events from homeserver");
 
         let response_text = {
+            let pubky_client =
+                PubkyClient::get().map_err(|e| EventProcessorError::client_error(e.to_string()))?;
             let url = format!(
                 "https://{}/events/?cursor={}&limit={}",
                 self.homeserver.id, self.homeserver.cursor, self.limit
             );
 
-            let response = reqwest::get(&url).await.map_err(|e| {
-                Box::new(EventProcessorError::PubkyClientError {
-                    message: format!("{:?}", e.source()),
-                })
-            })?;
+            let response = pubky_client
+                .client()
+                .request(Method::GET, &url)
+                .send()
+                .await
+                .map_err(|e| EventProcessorError::client_error(e.to_string()))?;
 
             response.text().await?
         };
